3fc41ee47daae6b9da8f4b4206adcb5deb659230
Johan Sebastian Perez <54086555+Hitoridake@users.noreply.github.com>
2020-02-24 14:20:29 -0500

    Update Primes.py

--- Ciclos/Primes.py
+++ Ciclos/Primes.py
@@ -2,17 +2,17 @@ import math
 number = float(input ("Enter a number : "))
 
 aux = 0
-i = 0
+i = 2
 
 if (number - int(number)) != 0 or number <= 1:
 	print("The number must be natural greater than 1")
 
 else:
 	
-	while aux == 0 and i < number
-		if (((number/i) - int(number/i)) != 0) 
-			aux ++
-		i++
+	while aux == 0 and i < number:
+		if (((number/i) - int(number/i)) == 0) :
+			aux = aux + 1
+		i = i + 1
 		
 	if aux == 0:
 		print("Your number is prime")
